3df5af6e26974edf3deeae107bd3c28dd306fd51
Dave Iverson <daveiverson@thoughtbot.com>
2026-02-11 15:43:04 -0600

    Updated the postinstall documentation

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -2,14 +2,14 @@ console.log(
   `thoughtbot/eslint-config installed! Use it by updating your ESLint configuration with:
 
   {
-    "extends": [
-      "@thoughtbot/eslint-config",
-      "@thoughtbot/eslint-config/typescript"
-    ]
+    extends: compat.extends(
+      '@thoughtbot/eslint-config',
+      '@thoughtbot/eslint-config/typescript',
+    )
   }
 
 Don't forget to also add a script in your package.json file:
 
   "lint": "eslint --max-warnings=0 --ext js,jsx,ts,tsx .",
-`
+`,
 );
